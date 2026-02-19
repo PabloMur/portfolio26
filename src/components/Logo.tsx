80baf530988b6a8b0342932a21d54b92c546d3d4
PabloMur <polillomurillo@gmail.com>
2026-02-18 22:25:41 -0300

    logo, letra agrandada

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
     return (
         <Link to="/" className="flex items-center gap-3 py-4 px-2 mb-6">
             <img src={logo} alt="PM Logo" className="w-20 h-20" />
-            <span className="text-xl font-semibold tracking-tight text-gray-800">Pablo Murillo</span>
+            <span className="text-2xl font-semibold tracking-tight text-gray-800">Pablo Murillo</span>
         </Link>
     );
 }
